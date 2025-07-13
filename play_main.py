ec6fa110ce18576482376e5aa5709934d8963d5f
Hendrik T. <hendrik@jw124.de>
2025-07-13 21:14:50 +0200

    added some extra lines (idk why either)

--- play_main.py
+++ play_main.py
@@ -5,8 +5,11 @@ from players import HumanPlayer
 from loading import load_rl_player, load_ga_player, get_builtin_bot
 from main import SCREEN_SIZE, GRID_WIDTH, GRID_HEIGHT, main
 
+
 # the trained models were trained with BaseEnv random start=False and also always as Player1 (as P2 they freak out)
 player2 = HumanPlayer(name="Human")
+
+
 
 # Choose ONE of the following:
 
@@ -21,6 +24,7 @@ player1 = load_rl_player("RL_5x5", "modelsaves/RL_7x7_input49_episode200.pth", i
 # Built-in bot example
 # player1 = get_builtin_bot("forward_bot", "ForwardBot")
 # player1 = get_builtin_bot("forward_bot", "ForwardBot")
+
 
 
 main(player1, player2, episodes=1_000_000, render=True, render_fps=10)
